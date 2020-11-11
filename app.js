8552f8d0534d67a6b44637176116367dbf6408df
jflukac <juanfranciscolukac@gmail.com>
2020-11-11 20:55:02 -0300

    add app.get to new html files

--- app.js
+++ app.js
@@ -24,8 +24,29 @@ app.get('/vender', function (req, res){
     res.sendFile(__dirname + '/views' + req.url + '.html')
 })
 
+app.get('/register', function (req, res){
+    res.sendFile(__dirname + '/views' + req.url + '.html')
+})
+
+app.get('/login', function (req, res){
+    res.sendFile(__dirname + '/views' + req.url + '.html')
+})
+
+app.get('/carrito', function (req, res){
+    res.sendFile(__dirname + '/views' + req.url + '.html')
+})
+
+app.post ('/', function (req, res) {
+    res.redirect('/')
+})
+
 // links a las imagenes
 app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/public/' + req.url)
+})
+
+// link al style sheet
+app.get('/css/stylesheet.css', function (req, res) {
     res.sendFile(__dirname + '/public/' + req.url)
 })
 
